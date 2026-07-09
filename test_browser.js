75b6261e33837023710e1951fa49df63574a6e3d
altamente.it <info@altamente.it>
2026-07-09 08:17:42 +0200

    d

---
+++ test_browser.js
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('BROWSER CONSOLE:', msg.text()));
+  page.on('pageerror', error => console.log('BROWSER ERROR:', error.message));
+  
+  await page.goto('http://localhost:5173/#/admin', { waitUntil: 'networkidle0' });
+  
+  await browser.close();
+})();
